34883618011f68216914b6d4cda75d3e7e2f0a3f
tegarmuhamad922@gmail.com <tegarmuhamad922@gmail.com>
2026-01-07 16:29:08 +0700

    test 11

--- src/views/AiPemasaran.vue
+++ src/views/AiPemasaran.vue
@@ -51,23 +51,7 @@ import { aiTableData } from '@/data/data.js'
                 target="_blank"
                 class="inline-flex items-center py-2 px-2 text-white justify-center w-9 h-9 rounded-lg bg-blue-500 hover:bg-blue-400 transition"
               >
-                <svg
-                  viewBox="0 0 64 64"
-                  xmlns="http://www.w3.org/2000/svg"
-                  stroke-width="3"
-                  stroke="#ffffff"
-                  fill="none"
-                >
-                  <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
-                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                  <g id="SVGRepo_iconCarrier">
-                    <path
-                      d="M55.4,32V53.58a1.81,1.81,0,0,1-1.82,1.82H10.42A1.81,1.81,0,0,1,8.6,53.58V10.42A1.81,1.81,0,0,1,10.42,8.6H32"
-                    ></path>
-                    <polyline points="40.32 8.6 55.4 8.6 55.4 24.18"></polyline>
-                    <line x1="19.32" y1="45.72" x2="54.61" y2="8.91"></line>
-                  </g>
-                </svg>
+                🔗
               </a>
             </td>
           </tr>
